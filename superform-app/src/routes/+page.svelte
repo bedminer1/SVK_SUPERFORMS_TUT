97e3c1de22a5b06f3bfb26d5c2e57b5de886721a
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-16 22:39:16 +0800

    use client side to make superForm object from data.form, simple display of form

--- superform-app/src/routes/+page.svelte
+++ superform-app/src/routes/+page.svelte
@@ -1,3 +1,18 @@
 <script lang="ts">
-	
+	import { superForm } from "sveltekit-superforms";
+
+	export let data
+
+	// superForm api makes data.form into an object with more useful properties
+	const { form } = superForm(data.form)
 </script>
+
+<form method="POST">
+	<label for="name" class="label">Name</label>
+	<input type="text" name="name" class="input" bind:value={$form.name} />
+  
+	<label for="email" class="label">E-mail</label>
+	<input type="email" name="email" class="input" bind:value={$form.email} />
+  
+	<div><button>Submit</button></div>
+</form>
